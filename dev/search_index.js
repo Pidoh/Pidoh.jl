6a9f8ad25efb85386b9432b17fe6f6796048c71c
Documenter.jl <documenter@juliadocs.github.io>
2020-11-03 18:48:12 +0000

    build based on a720998

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pidoh","category":"page"},{"location":"#Pidoh","page":"Home","title":"Pidoh","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pidoh]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pidoh","category":"page"},{"location":"#Pidoh","page":"Home","title":"Pidoh","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pidoh]","category":"page"},{"location":"#Pidoh.OneMax","page":"Home","title":"Pidoh.OneMax","text":"OneMax\n\nFitness function for binary strings.\n\nExamples\n\nConsider the following example:\n\n\n\n\n\n","category":"type"}]
 }
