c5a5177f63491880d06588ac36812dd136d0f113
Documenter.jl <documenter@juliadocs.github.io>
2020-11-03 18:27:53 +0000

    build based on e738f4d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pidoh","category":"page"},{"location":"#Pidoh","page":"Home","title":"Pidoh","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pidoh]","category":"page"}]
+}
